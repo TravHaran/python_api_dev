7d901fcacda719ac289d6b331c5f3d6b1d1a2760
TravHaran <travharan@yahoo.ca>
2022-08-12 22:04:42 -0400

    changed root welcome message

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ app.include_router(vote.router)
 
 @app.get("/") # decorator to convert function into fast api path operator
 async def root(): # path operation function
-    return {"message": "Pushing out to Ubuntu!"}
+    return {"message": "Welcome to my API!"}
     
 
 
